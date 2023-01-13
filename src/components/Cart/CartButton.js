ebaebdf887a78a5921c69fc70093a616a81895c8
Faiza Muskan <108839239+faiza-muskan@users.noreply.github.com>
2023-01-13 22:24:24 +0000

    added showCartHandler

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,17 @@
-import classes from './CartButton.module.css';
+import { useDispatch } from "react-redux";
+import { myCartAction } from "../../store/myCart-store";
+
+import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const showCartHandler = () => {
+    dispatch(myCartAction.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={showCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
